7a034048bdff033768e088ad58a6a637ecdc4187
JihadAG <106910410+JihadAG@users.noreply.github.com>
2022-11-06 20:04:07 +0200

    Add files via upload

---
+++ JS/script-config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          apBlue: '#4b94d1',
+          searchBarInput:'#faf4f9',
+          navHover:'#d3e2f5',
+          inputPlaceholder:'#4B94D1',
+          footerBgColor:'#4B94D1',
+          copyRightColor:'#FF9B39',
+          apDarkBlue:'#0a015b',
+        },
+        fontSize:{
+          jl:'500px'
+        }
+     
+      }
+    }
+    
+  }
+  
+  
---
+++ JS/script.js
@@ -0,0 +1,12 @@
+let darkModeToggle=document.getElementById('dark-mode');
+darkModeToggle.addEventListener('click',function(){
+    let bod=document.body;
+    bod.classList.toggle('dark');
+})
+let burgerMenu=document.getElementById('burger');
+let drpDwnMenu=document.getElementById('menu');
+
+burgerMenu.addEventListener('click',function(){
+    drpDwnMenu.classList.toggle('max-sm:invisible');
+})
+
